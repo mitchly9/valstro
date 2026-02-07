b028f923667e8146a2aac4d98b01fa813bfbf441
Mitchell <mitchly1@gmail.com>
2026-02-06 23:22:50 -0500

    Created CLI using Search API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log("Running");
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+import { disconnectSocket, initSocket, search } from "./api.js";
+import { SearchResult } from "./interface.js";
+
+try {
+  await initSocket();
+  console.log('Type "Exit" at any time to leave.');
+  console.log("A long time ago in a galaxy far, far away…");
+} catch (error) {
+  console.log(`Error occured ${error}`);
+}
+
+const rl = readline.createInterface({ input, output });
+
+try {
+  while (true) {
+    const answer: string = await rl.question(
+      "What character would you like to search for? ",
+    );
+
+    if (answer === "Exit") break;
+
+    try {
+      await search(answer, (result: SearchResult) => {
+        if (result.error) {
+          console.log(`ERROR: ${result.error}`);
+        } else {
+          console.log(
+            `(${result.page}/${result.resultCount}) ${result.name}  - [${result.films?.join(", ")}]`,
+          );
+        }
+      });
+    } catch (error) {
+      console.log(`Error occured ${error}`);
+    }
+  }
+} catch (error) {
+  console.log(`Error occured ${error}`);
+} finally {
+  console.log("May the Force be with you…");
+  rl.close();
+  disconnectSocket();
+}
